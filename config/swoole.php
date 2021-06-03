8c43d1e2a4912c71148f37d9644be3e5dea1ef4e
Shane <1109571639@qq.com>
2021-06-04 01:29:27 +0800

    更新

--- config/swoole.php
+++ config/swoole.php
@@ -3,27 +3,26 @@
 use think\swoole\websocket\socketio\Handler;
 
 return [
-//    'server'     => [
-
-//        'host'      => env('SWOOLE_HOST', '127.0.0.1'), // 监听地址
-//        'port'      => env('SWOOLE_PORT', 80), // 监听端口
-//        'mode'      => SWOOLE_PROCESS, // 运行模式 默认为SWOOLE_PROCESS
-//        'sock_type' => SWOOLE_SOCK_TCP, // sock type 默认为SWOOLE_SOCK_TCP
-//        'options'   => [
-//            'pid_file'              => runtime_path() . 'swoole.pid',
-//            'log_file'              => runtime_path() . 'swoole.log',
-//            'daemonize'             => true,
-//            // Normally this value should be 1~4 times larger according to your cpu cores.
-//            'reactor_num'           => swoole_cpu_num(),
-//            'worker_num'            => swoole_cpu_num(),
-//            'task_worker_num'       => swoole_cpu_num(),
-//            'enable_static_handler' => true,
-//            'document_root'         => root_path('public'),
-//            'package_max_length'    => 20 * 1024 * 1024,
-//            'buffer_output_size'    => 10 * 1024 * 1024,
-//            'socket_buffer_size'    => 128 * 1024 * 1024,
-//        ],
-//    ],
+    'server'     => [
+        'host'      => env('SWOOLE_HOST', '127.0.0.1'), // 监听地址
+        'port'      => env('SWOOLE_PORT', 9502), // 监听端口
+        'mode'      => SWOOLE_PROCESS, // 运行模式 默认为SWOOLE_PROCESS
+        'sock_type' => SWOOLE_SOCK_TCP, // sock type 默认为SWOOLE_SOCK_TCP
+        'options'   => [
+            'pid_file'              => runtime_path() . 'swoole.pid',
+            'log_file'              => runtime_path() . 'swoole.log',
+            'daemonize'             => true,
+            // Normally this value should be 1~4 times larger according to your cpu cores.
+            'reactor_num'           => swoole_cpu_num(),
+            'worker_num'            => swoole_cpu_num(),
+            'task_worker_num'       => swoole_cpu_num(),
+            'enable_static_handler' => true,
+            'document_root'         => root_path('public'),
+            'package_max_length'    => 20 * 1024 * 1024,
+            'buffer_output_size'    => 10 * 1024 * 1024,
+            'socket_buffer_size'    => 128 * 1024 * 1024,
+        ],
+    ],
     'websocket'  => [
         'enable'        => true,
         'handler'       => Handler::class,
