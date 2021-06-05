0dc61a38bfbfb68315b33b5633dc9cca65b53391
Shane <1109571639@qq.com>
2021-06-06 00:02:08 +0800

    更新

--- app/service/WS.php
+++ app/service/WS.php
@@ -1,61 +1,70 @@
 <?php
 /**
  *
- * @description: 永远爱颍
  *
- * @author: Shane
+ * @description:  大一大万大吉！
+ * @author: shenzheng
+ * @time: 2020/6/10 上午1:24
  *
- * @time: 2021/6/4 1:08
  */
 
-
-namespace app\service;
-
-use app\Request;
-use Swoole\Server;
-use think\swoole\Websocket;
-use app\common\business\lib\Redis;
-use Swoole\WebSocket\Frame;
-
-use think\Container;
+//cd /library/WebServer/Documents/swoole_test
 
 class WS
 {
 
-    protected $websocket = null;
-    protected $server = null;
+    private $ws = null;
 
-    public function __construct(Server $server, Websocket $websocket, Container $container)
-    {
-        $this->websocket = $websocket;//依赖注入的方式
-        $this->server = $server;
+    public function __construct(){
+        $this -> ws = new Swoole\WebSocket\Server("0.0.0.0", 9000);
+        $this -> ws -> set([
+            'task_worker_num' => 4,
+            'ssl_cert_file' => '/www/server/panel/vhost/cert/apptest.huihuagongxue.top/fullchain.pem',
+            'ssl_key_file' => '/www/server/panel/vhost/cert/apptest.huihuagongxue.top/privkey.pem'
+        ]);
+        $this -> ws -> on('open', [$this, "onOpen"]);
+        $this -> ws -> on('message', [$this, "onMessage"]);
+        $this -> ws -> on('task', [$this, "onTask"]);
+        $this -> ws -> on('finish', [$this, "onFinish"]);
+        $this -> ws -> on('close', [$this, "onClose"]);
+        $this -> ws -> start();
     }
 
-    public function onConnect(Request $request){}
-
-    // 监听客户连接
-    public function onOpen(Request $request)
-    {
-        $fd = $this->websocket->getSender();// 获取连接标识
-        $this->server->push($fd , 'something');// 发送给客户端
+    public function onOpen($ws, $request){
+        var_dump($request->fd, $request->get, $request->server);
+        $ws->push($request->fd, "欢迎客户端： {$request -> fd}\n");
     }
 
-    // 监听客户端发送消息
-    public function onMessage(Server $server, Frame $frame)
-    {
-        $fd = $frame->fd; // 为当前连接唯一值
-        $msg = json_decode($frame->data , true) ;// 获取cli上传的消息,json格式
-        $this->server->push($fd , $msg);// 发送给客户端
+    public function onMessage($ws, $frame){
+        echo "信息: {$frame -> data}\n";
+        foreach ($ws -> connections as $fd){
+            if ($fd == $frame -> fd){
+                $ws -> task([
+                    'fd' => $fd,
+                    'message' => "我: {$frame -> data}",
+                ]);
+                $ws -> push($fd, "我: {$frame -> data}");
+            }else{
+                $ws -> push($fd, "对方: {$frame -> data}");
+            }
+        }
     }
 
-    public function onPoint(Frame $frame, $data){
-        $fd = $frame->fd;
-        $this->server->push($fd , json_encode($data));
+    public function onTask($ws, $task_id, $src_worker_id, $data){
+        sleep(10);
+        return $data;
     }
 
-    //onClose触发的事件
-    public function onClose()
-    {
-        // 一般做一些资源释放的动作
+    public function onFinish($ws, $task_id, $data){
+        var_dump("task_id:" . $task_id);
+        var_dump($data);
     }
+
+    public function onClose($ws, $fd){
+        echo "客户端：{$fd} 关闭\n";
+    }
+
+
+
 }
+new WS();
