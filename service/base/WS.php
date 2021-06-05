4b8aab531fb7f8d7838b11327b513ed06cb6aad6
Shane <1109571639@qq.com>
2021-06-06 01:05:09 +0800

    更新

--- service/base/WS.php
+++ service/base/WS.php
@@ -25,41 +25,17 @@ class WS
         ]);
         $this -> ws -> on('open', [$this, "onOpen"]);
         $this -> ws -> on('message', [$this, "onMessage"]);
-//        $this -> ws -> on('task', [$this, "onTask"]);
-//        $this -> ws -> on('finish', [$this, "onFinish"]);
         $this -> ws -> on('close', [$this, "onClose"]);
         $this -> ws -> start();
     }
 
     public function onOpen($ws, $request){
         echo $request -> get['token'];
-        var_dump($request->fd, $request->get, $request->server);
-        $ws->push($request->fd, "欢迎客户端： {$request -> fd}\n");
+        $ws->push($request -> fd, "欢迎客户端： {$request -> fd}\n");
     }
 
     public function onMessage($ws, $frame){
-        echo "信息: {$frame -> data}\n";
-        foreach ($ws -> connections as $fd){
-            if ($fd == $frame -> fd){
-                $ws -> task([
-                    'fd' => $fd,
-                    'message' => "我: {$frame -> data}",
-                ]);
-                $ws -> push($fd, "我: {$frame -> data}");
-            }else{
-                $ws -> push($fd, "对方: {$frame -> data}");
-            }
-        }
-    }
-
-    public function onTask($ws, $task_id, $src_worker_id, $data){
-        sleep(10);
-        return $data;
-    }
-
-    public function onFinish($ws, $task_id, $data){
-        var_dump("task_id:" . $task_id);
-        var_dump($data);
+        $this -> chat -> handle($ws, $frame);
     }
 
     public function onClose($ws, $fd){
--- service/business/Chat.php
+++ service/business/Chat.php
@@ -12,8 +12,18 @@
 class Chat
 {
 
-    public function handle($uid, $target, $message){
-        echo json_encode($uid);
+    public function handle($ws, $frame){
+        foreach ($ws -> connections as $fd){
+            if ($fd == $frame -> fd){
+                $ws -> task([
+                    'fd' => $fd,
+                    'message' => "我: {$frame -> data}",
+                ]);
+                $ws -> push($fd, "我: {$frame -> data}");
+            }else{
+                $ws -> push($fd, "对方: {$frame -> data}");
+            }
+        }
     }
 
 }
