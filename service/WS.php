dde23f720cf95f54a76142c5d52d42d93229470b
Shane <1109571639@qq.com>
2021-06-16 09:49:16 +0800

    更新

--- service/WS.php
+++ service/WS.php
@@ -7,14 +7,18 @@
  * @time: 2020/6/10 上午1:24
  *
  */
+namespace service;
 
+use service\business\Chat;
 
 class WS
 {
 
     private $ws = null;
+    private $chat = NULL;
 
     public function __construct(){
+        $this -> chat = new Chat();
         $this -> ws = new Swoole\WebSocket\Server("0.0.0.0", 9502, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
         $this -> ws -> set([
             'task_worker_num' => 4,
@@ -23,40 +27,17 @@ class WS
         ]);
         $this -> ws -> on('open', [$this, "onOpen"]);
         $this -> ws -> on('message', [$this, "onMessage"]);
-        $this -> ws -> on('task', [$this, "onTask"]);
-        $this -> ws -> on('finish', [$this, "onFinish"]);
         $this -> ws -> on('close', [$this, "onClose"]);
         $this -> ws -> start();
     }
 
     public function onOpen($ws, $request){
-        var_dump($request->fd, $request->get, $request->server);
-        $ws->push($request->fd, "欢迎客户端： {$request -> fd}\n");
+        echo $request -> get['token'];
+        $ws->push($request -> fd, "欢迎客户端： {$request -> fd}\n");
     }
 
     public function onMessage($ws, $frame){
-        echo "信息: {$frame -> data}\n";
-        foreach ($ws -> connections as $fd){
-            if ($fd == $frame -> fd){
-                $ws -> task([
-                    'fd' => $fd,
-                    'message' => "我: {$frame -> data}",
-                ]);
-                $ws -> push($fd, "我: {$frame -> data}");
-            }else{
-                $ws -> push($fd, "对方: {$frame -> data}");
-            }
-        }
-    }
-
-    public function onTask($ws, $task_id, $src_worker_id, $data){
-        sleep(10);
-        return $data;
-    }
-
-    public function onFinish($ws, $task_id, $data){
-        var_dump("task_id:" . $task_id);
-        var_dump($data);
+        $this -> chat -> handle($ws, $frame);
     }
 
     public function onClose($ws, $fd){
@@ -67,3 +48,4 @@ class WS
 
 }
 new WS();
+echo __DIR__;
