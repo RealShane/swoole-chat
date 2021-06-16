f0a916f58cc359534f03b9a0f04414072d85d64e
Shane <1109571639@qq.com>
2021-06-16 11:33:25 +0800

    更新

--- service/base/WS.php
+++ service/base/WS.php
@@ -7,10 +7,9 @@
  * @time: 2020/6/10 上午1:24
  *
  */
-namespace service\base;
 require __DIR__ . '/../../public/index.php';
 use app\common\business\lib\Redis;
-use service\business\Chat;
+//use service\business\Chat;
 use Swoole\WebSocket\Server;
 
 class WS
