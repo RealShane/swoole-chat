8e0fd3613fb4f02e483e694bb3874903269f8d44
lichenying <2949794718@qq.com>
2021-06-17 10:36:42 +0800

    更新

--- app/api/controller/Chat.php
+++ app/api/controller/Chat.php
@@ -5,8 +5,16 @@ namespace app\api\controller;
 
 
 use app\BaseController;
+use WebSocket\Client;
 
 class Chat extends BaseController
 {
+
+    public function test() {
+        $client = new Client('wss://apptest.huihuagongxue.top:9502?token=' . $this -> getToken());
+        $client -> send("是的");
+        var_dump($client -> receive());
+        $client -> close();
+    }
 
 }
