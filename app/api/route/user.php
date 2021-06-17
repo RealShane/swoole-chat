83b33ec604f778bbd033471f93f5ebfc0aa69110
lichenying <2949794718@qq.com>
2021-06-17 09:08:01 +0800

    更新

--- app/api/route/user.php
+++ app/api/route/user.php
@@ -2,12 +2,6 @@
 use think\facade\Route;
 use app\api\middleware\IsLogin;
 
-Route::group('View/User', function (){
-    Route::rule('register', '/api/View/register', 'GET');
-    Route::rule('login', '/api/View/login', 'GET');
-    Route::rule('index', '/api/View/index', 'GET');
-});
-
 Route::group('User', function (){
     Route::rule('register', '/api/User/register', 'POST');
     Route::rule('login', '/api/User/login', 'POST');
--- config/view.php
+++ config/view.php
@@ -21,9 +21,6 @@ return [
     // 标签库标签开始标记
     'taglib_begin'  => '{',
     // 标签库标签结束标记
-    'taglib_end'    => '}',
+    'taglib_end'    => '}'
 
-    'tpl_replace_string' => [
-        '__API__' => '/assets'
-    ],
 ];
