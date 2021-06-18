b3da2698cca97f421832d92cb02523898480c4eb
lichenying <2949794718@qq.com>
2021-06-18 23:15:51 +0800

    更新

--- app/common/business/lib/Redis.php
+++ app/common/business/lib/Redis.php
@@ -15,6 +15,7 @@ class Redis
 
     public function __construct($store = 'redis'){
         $this -> setStore($store);
+        echo json_decode(config('cache.stores' . $this -> store));
         $this -> redis = new R(config('cache.stores' . $this -> store));
     }
 
